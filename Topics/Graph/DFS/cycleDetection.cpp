c96714d00e6583c5cdd584ebefb47af7b81670ca
AbuJubair7 <jubaircode7@gmail.com>
2023-12-18 16:15:42 +0600

    DFS code updated

--- Topics/Graph/DFS/cycleDetection.cpp
+++ Topics/Graph/DFS/cycleDetection.cpp
@@ -3,16 +3,23 @@ using namespace std;
 
 vector<int> adj[100]; // adjacency list
 int visited[100];
+int grey[100];
 stack<int> st;
 
-void dfs(int u) {
+void dfs(int p, int u) {
   if (visited[u]) {
     return;
+  } else if (grey[u]) {
+    cout << "Cycle at: " << u << '\n';
+    return;
+  }
+  grey[u] = 1;
+  for (auto v : adj[u]) {
+    if (v != p) {
+      dfs(u, v);
+    }
   }
   visited[u] = 1;
-  for (auto v : adj[u]) {
-    dfs(v);
-  }
   st.push(u);
 }
 
@@ -22,13 +29,14 @@ int main() {
   int n, e;
   cin >> n >> e;
   memset(visited, 0, sizeof visited);
+  memset(grey, 0, sizeof grey);
   for (int i = 1; i<=e; i++) {
     int u, v;
     cin >> u >> v;
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
-  dfs(1);
+  dfs(-1, 1);
   while (!st.empty()) {
     cout << st.top() << '\n';
     st.pop();
