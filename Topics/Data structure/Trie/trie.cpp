f828c947823a7001eff02495bb47ac35b609d112
AbuJubair7 <jubaircode7@gmail.com>
2024-03-20 14:14:29 +0600

    trie updated

--- Topics/Data structure/Trie/trie.cpp
+++ Topics/Data structure/Trie/trie.cpp
@@ -1,17 +1,3 @@
-/*
-Author: jubair7
-Date: 20/3/24
-*/
-#include<bits/stdc++.h>
- 
-using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 struct node {
   node* adj[26 + 7];
   bool end;
@@ -24,23 +10,19 @@ struct node {
 } * root;
 
 void insert (string s) {
-  int len = s.length();
   node* curr = root;
-  for (int i = 0; i<len; i++) {
+  for (int i = 0; i<s.length(); i++) {
     int id = s[i] - 'a';
-    if (curr->adj[id] == NULL) {
+    if (curr->adj[id] == NULL)
       curr->adj[id] = new node();
-    }
-    debug(i);
     curr = curr->adj[id];
   }
   curr->end = true;
 }
 
 bool find (string s) {
-  int len = s.length();
   node* curr = root;
-  for (int i = 0; i<len; i++) {
+  for (int i = 0; i<s.length(); i++) {
     int id = s[i] - 'a';
     if (curr->adj[id] == NULL)
       return false;
@@ -49,15 +31,13 @@ bool find (string s) {
   return curr->end;
 } 
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    string s;
-    cin >> s;
-    insert(s);
-    
+bool findP (string s) {
+  node* curr = root;
+  for (int i = 0; i<s.length(); i++) {
+    int id = s[i] - 'a';
+    if (curr->adj[id] == NULL)
+      return false;
+    curr = curr->adj[id];
   }
+  return true;
 }
