c65ccbfa2d3b5a29e3a88d92e58fa134e0a35007
AbuJubair7 <jubaircode7@gmail.com>
2024-03-20 12:26:15 +0600

    trie updated

--- Topics/Data structure/Trie/trie.cpp
+++ Topics/Data structure/Trie/trie.cpp
@@ -13,40 +13,40 @@ using namespace std;
 #endif
 
 struct node {
-	node* adj[26 + 7];
-	bool end;
-	node () {
-		end = false;
-		for (int i = 1; i<=26; i++) {
-			this->adj[i] = NULL;
-		}
-	}
+  node* adj[26 + 7];
+  bool end;
+  node () {
+    end = false;
+    for (int i = 1; i<=26; i++) {
+      this->adj[i] = NULL;
+    }
+  }
 } * root;
 
 void insert (string s) {
-	int len = s.length();
-	node* curr = root;
-	for (int i = 0; i<len; i++) {
-		int id = s[i] - 'a';
-		if (curr->adj[id] == NULL) {
-			curr->adj[id] = new node();
-		}
-		debug(i);
-		curr = curr->adj[id];
-	}
-	//curr->end = true;
+  int len = s.length();
+  node* curr = root;
+  for (int i = 0; i<len; i++) {
+    int id = s[i] - 'a';
+    if (curr->adj[id] == NULL) {
+      curr->adj[id] = new node();
+    }
+    debug(i);
+    curr = curr->adj[id];
+  }
+  curr->end = true;
 }
 
 bool find (string s) {
-	int len = s.length();
-	node* curr = root;
-	for (int i = 0; i<len; i++) {
-		int id = s[i] - 'a';
-		if (curr->adj[id] == NULL)
-			return false;
-		curr = curr->adj[id];
-	}
-	return curr->end;
+  int len = s.length();
+  node* curr = root;
+  for (int i = 0; i<len; i++) {
+    int id = s[i] - 'a';
+    if (curr->adj[id] == NULL)
+      return false;
+    curr = curr->adj[id];
+  }
+  return curr->end;
 } 
 
 int main() {
@@ -55,9 +55,9 @@ int main() {
   int tt;
   cin >> tt;
   while (tt--) {
-  	string s;
-  	cin >> s;
-  	insert(s);
-  	
+    string s;
+    cin >> s;
+    insert(s);
+    
   }
 }
