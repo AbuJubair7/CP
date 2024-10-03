abdd5aac9d7abac69111b0bd9755f0edc07ff11d
AbuJubair7 <jubaircode7@gmail.com>
2024-10-03 22:21:10 +0600

    NT added

--- Topics/Number theory/Euclidean/extended_gcd.cpp
+++ Topics/Number theory/Euclidean/extended_gcd.cpp
@@ -1,19 +1,19 @@
-int gcd(int a, int b, int& x, int& y) {
+int extended_gcd(int a, int b, int &x, int &y) {
   if (b == 0) {
-      x = 1;
-      y = 0;
-      return a;
+    x = 1;
+    y = 0;
+    return a;
   }
   int x1, y1;
-  int d = gcd(b, a % b, x1, y1);
+  int gcd = extended_gcd(b, a % b, x1, y1);
   x = y1;
-  y = x1 - y1 * (a / b);
-  return d;
+  y = x1 - (a / b) * y1;
+  return gcd;
 }
 
 int inverse(int a, int m) {
   int x, y;
-  int g = extended_euclid(a, m, x, y);
+  int g = extended_gcd(a, m, x, y);
   if (g != 1) return -1;
   return (x % m + m) % m;
 }
