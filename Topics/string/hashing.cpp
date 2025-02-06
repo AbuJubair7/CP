dc10439f8e04882a22fb691805d87db23d356e5d
AbuJubair7 <jubaircode7@gmail.com>
2025-02-06 19:32:04 +0600

    Hashing updated

--- Topics/string/hashing.cpp
+++ Topics/string/hashing.cpp
@@ -1,50 +1,47 @@
 const int N = 2e5 + 7;
 
-const int b1 = 37, b2 = 177, mod1 = 1000000007, mod2 = 987654319;
-pair<long long, long long> P[N], IP[N];
+const int b1 = 137, b2 = 277, mod1 = 1000000007, mod2 = 987654319;
+pair<int, int> P[N], IP[N];
 
-long long binpow (long long a, long long b, long long m) {
-  long long res = 1;
+int binpow (long long a, long long b, int m) {
+  int res = 1;
   while (b) {
-    if (1 & b) res = (res*a)%m;
-    a = (a*a)%m;
+    if (1 & b) res = (1LL * res * a) % m;
+    a = (1LL * a * a) % m;
     b >>= 1;
   }
   return res;
 };
 
 void preCal(){
-  long long ib1 = binpow(b1, mod1-2, mod1); // using fermat's little theorem a^(p-2)=a^-1 (mod p)
-  long long ib2 = binpow(b2, mod2-2, mod2);
+  int ib1 = binpow(b1, mod1-2, mod1); // using fermat's little theorem a^(p-2)=a^-1 (mod p)
+  int ib2 = binpow(b2, mod2-2, mod2);
   P[0] = {1, 1};
   IP[0] = {1, 1};
-  for (int i = 1; i<N; i++) {
-    P[i] = {(b1 * P[i-1].first)%mod1, (b2 * P[i-1].second)%mod2};
-    IP[i] = {(ib1 * IP[i-1].first)%mod1, (ib2 * IP[i-1].second)%mod2};
+  for (int i = 1; i < N; i++) {
+    P[i] = {(1LL * b1 * P[i-1].first)%mod1, (1LL * b2 * P[i-1].second)%mod2};
+    IP[i] = {(1LL * ib1 * IP[i-1].first)%mod1, (1LL * ib2 * IP[i-1].second)%mod2};
   }
 }
 
 struct Hash{
-  vector<pair<int, int>> val;
+  pair<int, int> val[N];
   int n;
   Hash (){}
-  Hash(string s) {
-    val.emplace_back(0, 0);
+  void build(string s) {
+    val[0] = {0, 0};
     n = s.length();
-    for (int i = 0; i<n; i++) {
-      int k = s[i]-'a'+1;
-      int h1 = ((val[i].first + k * P[i].first) % mod1) % mod1;
-      int h2 = ((val[i].second + k * P[i].second) % mod2) % mod2;
-      val.push_back({h1, h2});
+    for (int i = 0; i < n; i++) {
+      int k = s[i];
+      int h1 = ((1LL * k * P[i].first + val[i].first) % mod1) % mod1;
+      int h2 = ((1LL * k * P[i].second + val[i].second) % mod2) % mod2;
+      val[i + 1] = {h1, h2};
     }
   }
-  pair<int, int> get(int l, int r) {
+  pair<int, int> get_hash(int l, int r) {
     pair<int, int> curr;
-    curr.first = ((val[r].first - val[l-1].first + mod1) * IP[l-1].first) % mod1;
-    curr.second = ((val[r].second - val[l-1].second + mod2) * IP[l-1].second) % mod2;
+    curr.first = (1LL * (val[r].first - val[l-1].first + mod1) * IP[l-1].first) % mod1;
+    curr.second = (1LL * (val[r].second - val[l-1].second + mod2) * IP[l-1].second) % mod2;
     return curr;
-  }
-  pair<int, int> get() {
-    return get(1, n);
   } 
-};
+} h, reverse_hash;
